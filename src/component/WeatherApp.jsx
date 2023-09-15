2e2401827ad596220dc2f64eaaa1b58dc4ff01a2
AndreaFerrara13 <138196137+AndreaFerraray@users.noreply.github.com>
2023-09-15 12:11:06 +0200

    WeatherApp

---
+++ src/component/WeatherApp.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function WeatherApp() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+
+  const apiKey = "b6393e616b5d02fc3bdd3fa313d9d494";
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&cnt=5`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error("Errore nella ricerca del meteo:", error);
+      setWeatherData(null);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Meteo App</h1>
+      <input
+        type="text"
+        placeholder="Inserisci il nome della città"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button onClick={handleSearch}>Cerca</button>
+      {weatherData && (
+        <div>
+          <h2>{weatherData.city.name}</h2>
+          <ul>
+            {weatherData.list.map((forecast) => (
+              <li key={forecast.dt}>
+                <p>Data e Ora: {new Date(forecast.dt * 1000).toLocaleString()}</p>
+                <p>Temperatura: {forecast.main.temp}°C</p>
+                <p>{forecast.weather[0].description}</p>
+                <i className={`wi wi-owm-${forecast.weather[0].id}`} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default WeatherApp;
